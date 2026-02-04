a63130df8466994a76f4d12a9844457ee36bcac0
joelatwo <joelatwo@gmail.com>
2026-02-03 18:10:09 -0700

    add base config

--- next.config.ts
+++ next.config.ts
@@ -6,8 +6,8 @@ const nextConfig: NextConfig = {
     unoptimized: true,
   },
   // If your repository name is not the root, uncomment and set the basePath
-  // basePath: process.env.NODE_ENV === "production" ? "/solo_clover" : "",
-  // assetPrefix: process.env.NODE_ENV === "production" ? "/solo_clover" : "",
+  basePath: process.env.NODE_ENV === "production" ? "/solo_clover" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/solo_clover" : "",
 };
 
 export default nextConfig;
